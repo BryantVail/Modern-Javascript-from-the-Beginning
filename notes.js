fe8252df09e113ea51f949506342ad795add69d2
Bryant Vail <BryantVail@gmail.com>
2019-08-29 17:48:46 -0400

    chapter 2 begins

---
+++ notes.js
@@ -0,0 +1,124 @@
+({
+  url: "https://subscription.packtpub.com/video/web_development/9781789539509",
+  doctype: "notes, note",
+
+  chapters: [
+    {
+      number: 1,
+      name: "intro, getting started",
+      sections: [
+        {
+          name: "Welcome to the course",
+          desc: "",
+          notes: [
+            "starts early",
+            "async & other complex features",
+            "prototypes & classes",
+            "console for most output, but also UI interaction",
+            "entire front end to a REST api",
+            "REST Library using 'fetch"
+          ],
+          sections: [
+            {
+              name: "why take this course",
+              reasons: [
+                "learn to build 'without' frameworks",
+                "get better at building 'with' frameworks by understanding vanillaJS",
+                "DOM: learn your way around the DOM without using jQuery",
+                "design patterns to structure applications"
+              ]
+            },
+            {
+              name: "some topics covered",
+              topics: [
+                "basics and fundamentals",
+                "DOM manipulation & UI Events",
+                "OOP; ES5 Prototypes + ES6 Classes",
+                "Async JS; Ajax, Fetch, Promises, Async/Await",
+                "error handling, regular expressions, localStorage",
+                "Programming Scope, Arrow Functions, Map, Set, Iterators",
+                "Javascript Patterns",
+                "~10 Projects"
+              ]
+            },
+            {
+              name: "projects",
+              projects: [
+                "task list (DOM & LS)",
+                "Loan Calculator",
+                "Numbers Guessing Game",
+                "Book Manager (OOP intro)",
+                "EasyHTTP library (fetch api)",
+                "GitHub Finder (github api)",
+                "Weather App (weatherUnderground API)",
+                "TraCalorie (Module Pattern)",
+                "Crud App with ES6 Modules (Webpack)",
+                {
+                  name: "Chuck norris joke generator",
+                  type: "mini"
+                },
+                {
+                  name: "profile scroller (iterators)",
+                  type: "mini"
+                },
+                {
+                  name: "State Pattern Interface",
+                  type: "mini"
+                }
+              ]
+            },
+            {
+              name: "other skills learned"
+            }
+          ]
+        }, //end chapter section
+        {
+          name: "Project Files and Questions",
+          desc: "",
+          notes: [
+            "project files for the video",
+            "modular learning sections usually just have index.html & app.js",
+            "projects have incremental files"
+          ],
+          sections: [
+            {
+              name: "code style",
+              desc: "Practice is the best way to learn",
+              notes: [
+                "practices get better as course progresses",
+                "everyone programs differently"
+              ]
+            }
+          ]
+        }, //end project files and questions
+        {
+          name: "visual studio code setup",
+          desc: "",
+          notes: [
+            "webpack & babel on the last project",
+            "liveServer",
+            "color bracket",
+            "javscript snippets"
+          ]
+        }
+      ] //end chapter1 sections
+    }, //end chapter1 intro
+    {
+      name: "intro and getting started",
+      desc: "",
+      sections: [
+        {
+          name: "section intro & file setup",
+          desc: "",
+          notes: ["from scratch", "data types", "conversions"],
+          aspects: [
+            {
+              name: "script tags",
+              desc: "inline javascript or refer to a javascript file"
+            }
+          ]
+        }
+      ]
+    }
+  ] //end chapters
+});
