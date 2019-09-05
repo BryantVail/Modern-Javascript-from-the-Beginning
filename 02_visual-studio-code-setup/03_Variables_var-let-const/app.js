db3991c22ff963636528fab11f283265fd06bfea
Bryant Vail <BryantVail@gmail.com>
2019-09-05 17:52:59 -0400

    variables complete

--- 02_visual-studio-code-setup/03_Variables_var-let-const/app.js
+++ 02_visual-studio-code-setup/03_Variables_var-let-const/app.js
@@ -1,7 +1,57 @@
 //Variables: [var, let, const];
 
-//var
+// //var
+// //  since the beginning
+// //  will not be used after this video
+// var name = "John Doe";
+// console.log(name);
+// // re-assign
+// name = "new name";
+// console.log(name);
 
-//let
+// //  declaration without assignment
+// var greeting;
+// //  undefined
+// console.log(greeting);
+// greeting = "hello";
+// console.log(greeting);
 
-//const
+// //variable conventions:
+// //  letters, numbers, _, $
+// //  cannot start a variable name with a number
+
+// var letters = "letters";
+// var numbers = "letter1";
+// //  underscore is not advised
+// var _global = "global";
+
+// //  multi-word
+// var firstName = "camelCase";
+// var PascalCase = "PascalCase";
+
+//LET
+
+//CONST
+//  value must be assigned
+//  use as often as possible when values should not change
+const name = "firstname";
+console.log(name);
+
+// name = "new name";// typeError
+
+//const greeting;// must be initialized with value
+
+//  how const can be changed
+const person = {
+  name: "John"
+};
+console.log(person);
+//  change name of object, const is still an object
+person.name = "new name";
+console.log(person);
+
+//  Arrays
+const numbers = [1, 2, 3, 4, 5];
+console.log(numbers);
+numbers.push(100);
+console.log(numbers);
