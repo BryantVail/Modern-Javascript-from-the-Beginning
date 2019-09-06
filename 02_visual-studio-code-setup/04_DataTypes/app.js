ce886336714c9250917c654948091b37724da29f
Bryant Vail <BryantVail@gmail.com>
2019-09-06 17:54:17 -0400

    Data Types in Javascript complete

--- 02_visual-studio-code-setup/04_DataTypes/app.js
+++ 02_visual-studio-code-setup/04_DataTypes/app.js
@@ -1,1 +1,38 @@
 //DataTypes
+
+//  PRIMITIVE
+//    stack stored
+
+//    String
+const name = "John";
+
+//    Number
+const age = 29;
+
+//    Boolean
+const hasKids = false;
+
+//    Null
+const house = null; //returns 'object'
+
+//    Undefined
+let test;
+
+//    Symbols (ES6)
+const sym = Symbol();
+
+//  REFERENCE
+//    Heap Stored
+//    Pointer
+const hobbies = ["movies", "music"];
+
+const address = {
+  number: "680",
+  street: "Muscovy",
+  streetSuffix: "circle",
+  city: "Deland",
+  state: "florida",
+  zip: "32825"
+};
+
+console.log(typeof hobbies);
